affa0515ddfec6b9cbf4fb2685ba32025e7e0566
Maddy Foley <maddyfoley5@gmail.com>
2025-10-09 08:28:23 -0400

    adding sass to handle theme

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Basic Custom Bingo for Family
 
 Flask & HTML template Application
 
+Requires sass installation (this project using a host-machine library)
+
 Make a Bingo Card
 ```python card.py```
 
--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
 from card import make_detail_card_rows
 
 
 
 app = Flask(__name__, static_folder='static')
 
-@app.route("/")
+@app.route("/", methods=['GET'])
 def hello_world():
    card = make_detail_card_rows()
-   
+
    return render_template('card.html', card=card)
 
 
