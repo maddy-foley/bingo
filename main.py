1ffee6b4056ed3ff77a02d4ba4379132f2503baf
Maddy Foley <maddyfoley5@gmail.com>
2025-10-10 16:20:35 -0400

    created homepage

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ from card import make_detail_card_rows
 app = Flask(__name__, static_folder='static')
 
 @app.route("/")
-def hello_world():
+def home():
+   return render_template('home.html')
+
+@app.route("/card")
+def card():
    card = make_detail_card_rows()
 
    return render_template('card.html', card=card)
-
-
