ccd67f9a9929d9e034b7827348e22498fd8657f6
Maddy Foley <maddyfoley5@gmail.com>
2025-10-13 11:34:04 -0400

    boxes ligning up with card

--- scribus_fill_card.py
+++ scribus_fill_card.py
@@ -2,26 +2,90 @@ import scribus
 from make_card import make_detail_card_rows
 from filename import filename
 
-origX = 1.2
-origY = 5.2
 
-my_card = make_detail_card_rows(filename)
-
-box_width=5
-box_height=5
-
+def draw_content_boxes(cell_style):
+    origX = cell_style['origX']
+    origY = cell_style['origY']
+    cell_width = cell_style['cell_width']
+    cell_height = cell_style['cell_height']
 # 5 columns
-for i in range(5):
-    column = f"column{i}"
-    scribus.createText(origX,origY,1,6,column)
-    scribus.setTextAlignment(scribus.ALIGN_CENTERED, column)
-    scribus.setLineColor("Black",column)
-    scribus.setFontSize(18,column)
-    scribus.setFont("Roman", column)
-    for j in range(5):
-        scribus.insertText(my_card[i+j], -1, column)
-        scribus.insertText( "\n\n",-1, column)
-        origY += 1.2
-    break
-    origY = 6
-    origX += 1.2
+    for i in range(25):
+        if i % 5 == 0:
+            origY = cell_style['origY']
+            origX +=  cell_style["orig_shiftX"]
+
+        cell = f"cell{i}"
+        scribus.createText(origX,origY,cell_width,cell_height,cell)
+        scribus.setTextAlignment(scribus.ALIGN_CENTERED, cell)
+        scribus.setLineColor(cell_style['font_color'],cell)
+        scribus.setFontSize(cell_style['font_size'],cell)
+
+        # Font is not included with scribus and needs to be downloaded to OS system manually
+        scribus.setFont(cell_style['font_name'], cell)
+        origY +=  cell_style["orig_shiftY"]
+
+
+def fill_box_with_text():
+    my_card = make_detail_card_rows(filename)
+    for i in range(25):
+        cell = f"cell{i}"
+        scribus.insertText(my_card[i], -1, cell)
+
+def make_title_boxes(cell_style):
+    origX = cell_style['origX']
+    origY = cell_style['origY']
+
+    for letter in ["B","I","N","G","O"]:
+        cell = f"{letter}"
+        scribus.createText(origX,origY, cell_style['cell_width'], cell_style['cell_height'], cell)
+        scribus.insertText(letter, -1, cell)
+        scribus.setTextAlignment(scribus.ALIGN_CENTERED, cell)
+        scribus.setLineColor(cell_style['font_color'],cell)
+        scribus.setFontSize(cell_style['font_size'],cell)
+        scribus.setFont(cell_style['font_name'], cell)
+        origX += cell_style["orig_shiftX"]
+
+def delete_all_boxes():
+    for letter in ["B","I","N","G","O"]:
+        title_cell = f"{letter}"
+        if scribus.objectExists(title_cell):
+            scribus.deleteText(title_cell)
+            scribus.deleteObject(title_cell)
+    for i in range(25):
+        cell = f"cell{i}"
+        if scribus.objectExists(cell):
+            scribus.deleteText(cell)
+            scribus.deleteObject(cell)
+
+
+
+title_cell_style = {
+    "font_name":"Ultra Regular" ,
+    "font_color":"Black",
+    "font_size": 30,
+    "origX":1.25,
+    "origY":4.1,
+    "cell_width": 1.1,
+    "cell_height":1,
+    "alignment": scribus.ALIGN_CENTERED,
+    "orig_shiftX": 1.2,
+    "orig_shiftY": 0
+}
+
+other_cell_style = {
+    "font_name": "EB Garamond Bold",
+    "font_color":"Black",
+    "font_size": 18,
+    "origX":0,
+    "origY":4.9,
+    "cell_width": 1.1,
+    "cell_height":1,
+    "alignment": scribus.ALIGN_CENTERED,
+    "orig_shiftX": 1.2,
+    "orig_shiftY": 1.1 
+}
+# dev delete boxes to quickly redo-boxes
+delete_all_boxes()
+make_title_boxes(title_cell_style)
+# draw_content_boxes(other_cell_style)
+fill_box_with_text()
