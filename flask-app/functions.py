0c9cd33617318714df6991341be6f669b2977c2d
Maddy Foley <maddyfoleydev@gmail.com>
2025-10-22 23:30:41 -0400

    layout basic flask app

--- flask-app/functions.py
+++ flask-app/functions.py
@@ -1,9 +1,41 @@
 import random
 import json
 
-def draw_bingo_card():
+def nums_to_card(numbers):
+    data = get_bingo_data()
     keys = ["b","i","n","g","o"]
+    res = [None] * 75
+
+    for i in numbers:
+        key = keys[i//15]
+        res[i] = data[key][i % 15]
+    return res
+
+def draw_bingo_card():
+
     random_integer = random.randint(1, 75)
+    data = get_drawn_card_data()
+    while random_integer in data:
+        random_integer = random.randint(1, 75)
+    data.append(random_integer)
+    save_drawn_card_data(data)
+
+    return nums_to_card(data)
+
+
+def get_drawn_card_data():
+    with open('flask-app/data/drawn_cards.json','r') as file:
+        data = json.load(file)
+        file.close()
+        return data
+
+def save_drawn_card_data(data):
+    with open('flask-app/data/drawn_cards.json','w') as file:
+        json.dump(data,file,indent=4)
+        file.close()
+
+def get_bingo_data():
     with open('data/bingo.json', 'r') as file:
         data = json.load(file)
-        print(data[keys[random_integer % 5]][random_integer % 15])
+        file.close()
+        return data
--- flask-app/main.py
+++ flask-app/main.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from functions import draw_bingo_card
 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    draw_bingo_card()
-    return render_template('bingo.html')
+@app.route('/')
+def home():
+    cards = draw_bingo_card()
+    return render_template("bingo.html",cards=cards)
+# @app.get('/')
+# def get_card():
+#     return "hi"
+
+# @app.post('/login')
+# def draw_card():
+#     return "d"
